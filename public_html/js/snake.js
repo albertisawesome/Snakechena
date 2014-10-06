2df06779985b7e18673ed47cca53a914415b6eae
Albert Chen <chenalbert60@gmail.com>
2014-10-06 15:02:51 -0700

    I am on video snake 10

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,3 +1,19 @@
+var  snake;
 
 
+var context;
+var screenWidth;
+var screenHeight;
 
+function gameInitialize(){
+    var canvas=document.getElementById("game-screen");
+    context=canvas.getContext("2d");
+}
+
+function gameLoop(){    
+
+}
+
+function gameDraw(){
+    
+}
