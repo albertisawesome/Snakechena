0e01bca016ab77b0df21b4c96b96b88eaec50695
Albert Chen <chenalbert60@gmail.com>
2014-10-07 15:02:24 -0700

    i have made a screen color

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,10 +4,18 @@ var  snake;
 var context;
 var screenWidth;
 var screenHeight;
-
+gameInitialize();
+gameDraw();
 function gameInitialize(){
     var canvas=document.getElementById("game-screen");
     context=canvas.getContext("2d");
+
+    screenWidth=window.innerWidth;
+    screenHeight=window.innerHeight;
+    
+    canvas.width= screenWidth;
+    canvas.height= screenHeight;
+
 }
 
 function gameLoop(){    
@@ -15,5 +23,6 @@ function gameLoop(){
 }
 
 function gameDraw(){
-    
+   context.fillStyle = "rgb(100,105,203,133)";
+   context.fillRect(0, 0, screenWidth, screenHeight);
 }
